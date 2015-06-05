2abab0fa20bdd26aa71e511c823d1a0b9a910fdf
john <jayelwhy@yahoo.com>
2015-06-05 11:22:30 -0700

    include an index page with the 'add new team function'

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require('sinatra')
 require('sinatra/reloader')
-# require('./lib/team')
+require('./lib/team')
 require('./lib/member')
 also_reload('lib/**/*.rb')
 
 get('/') do
+  @teams = Team.all
+  erb(:index)
+end
+
+post('/') do
+  team = params.fetch('team')
+  Team.new(team).save
+  @teams = Team.all
   erb(:index)
 end
